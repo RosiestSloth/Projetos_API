4b9c61f79f0c2e9d3ba9ce797f964169561eca88
Vinicius Santos <165097853+RosiestSloth@users.noreply.github.com>
2025-10-06 19:03:04 -0300

    Adição de UserRequests
    Adicionei mais rotas para users, e também adicionei um tratamento de erros durante a adição de usuários.

--- Pizzaria/app/Http/Controllers/Api/UserController.php
+++ Pizzaria/app/Http/Controllers/Api/UserController.php
@@ -3,17 +3,73 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UserRequest;
 use App\Models\User;
+use Exception;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
-    public function index() 
+    public function index() : JsonResponse
     {
+        // Requisitar os usuários
         $users = User::orderBy('id', 'DESC')->get();
-
+        // Retornar a variável com os valores
         return response()->json([
             'status' => true,
             'users' => $users,
         ], 200);
+    }
+
+    public function show(User $user) : JsonResponse
+    {
+        // Retornar a variável com os valores
+        return response()->json([
+            'status' => true,
+            'user' => $user,
+        ], 200);
+    }
+
+    public function store(UserRequest $request) : JsonResponse
+    {
+        DB::beginTransaction();
+
+        try{
+
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => $request->password,
+            ]);
+
+            DB::commit();
+
+            return response()->json([
+                'status' => true,
+                'user' => $user,
+                'message' => "Usuário cadastrado com sucesso!",
+            ], 201);
+            
+
+        }catch (Exception $e) {
+            // Operação não concluída
+
+            DB::rollBack();
+
+            return response()->json([
+                'status' => false,
+                'message' => "Usuário não cadastrado!",
+            ], 400);
+        }
+    }
+
+    public function update(UserRequest $request) : JsonResponse
+    {
+        return response()->json([
+                'status' => true,
+                'user' => $request,
+                'message' => "Usuário editado com sucesso!",
+            ], 200);
     }
 }
---
+++ Pizzaria/app/Http/Requests/UserRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'erros' => $validator->errors()
+        ], 422));
+    }
+
+    
+    public function rules(): array
+    {
+        // Recuperar o id do usuário enviado na URL
+        $userId = $this->route('user');
+
+        return [
+            'name' => 'required',
+            'email' => 'required|email|unique:users,email,' . ($userId ? $userId->id : null),
+            'password' => 'required|min:6',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'name.required' => 'Campo nome é obrigatório!',
+            'email.required' => 'Campo email é obrigatório!',
+            'name.email' => 'Necessário enviar e-mail válido!',
+            'name.unique' => 'O e-mail já está cadastrado!',
+
+            
+            'password.required' => 'Campo senha é obrigatório!',
+            'password.min' => 'Senha com no mínimo :min caracteres!',
+        ];
+    }
+}
